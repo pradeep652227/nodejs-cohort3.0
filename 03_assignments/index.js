ce3235f0238cdfb50bd60845fa980e165f3fd6c8
Pradeep Gaur <ps652227@gmail.com>
2024-09-28 22:14:32 +0530

    middleware assign 03 - Middleware Error Handler

--- 03_assignments/index.js
+++ 03_assignments/index.js
@@ -1,14 +1,22 @@
+const express=require('express');
 
 //Importing internal modules
-const app=require('./modules/rateLimitter');
+const app=express();
 
 //essential instances and variables
 const PORT=3000 || process.env.PORT;
 
+//middlewares
 
 //routes
 app.get('/users',(req,res)=>{
+   throw new Error('Something Went Wrong');
     res.send('Secret Rotue!!Only accessed by our array of users');
+});
+
+
+app.use((err,req,res,next)=>{
+    res.status(404).json({error:err});
 });
 
 app.listen(PORT,()=>{
